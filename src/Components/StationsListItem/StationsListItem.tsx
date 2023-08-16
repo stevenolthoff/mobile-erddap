2094bc40a08310752bd9ef30fb7c73e57e839fb2
Steven Olthoff <steven@axiomdatascience.com>
2023-08-16 15:09:33 -0400

    fix station list item styling

--- src/Components/StationsListItem/StationsListItem.tsx
+++ src/Components/StationsListItem/StationsListItem.tsx
@@ -10,7 +10,7 @@ export default function StationsListItem ({ title: Title, summary: Summary, chil
   return (
     <div className='px-4 py-2 cursor-pointer hover:bg-slate-200
       active:bg-slate-300 active:text-blue-500 select-none
-      flex max-w-full gap-2
+      flex max-w-full w-full gap-2 justify-between
     '>
       <div className='truncate ...'>
         <p className='text-lg text-black-800 font-medium h-6 truncate ...'>{Title}</p>
