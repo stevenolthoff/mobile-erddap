f16a4740e876af65e61732c74d43fe3fd4cd8ae1
Steven Olthoff <steven@axiomdatascience.com>
2023-09-07 16:12:48 -0400

    WIP: displaying chart scrub data

--- src/Components/Sensor/Sensor.tsx
+++ src/Components/Sensor/Sensor.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import React, { type ReactElement, useState, useEffect } from 'react'
-import { Chart, EPlotTypes, type IPlot } from '@axdspub/axiom-charts'
+import { Chart, EPlotTypes, IPlotScrubPosition, IPlotScrubPositions, type IPlot } from '@axdspub/axiom-charts'
 import FavoriteButton from '@/Components/FavoriteButton/FavoriteButton'
 import { ISensor } from '@/Contexts/FavoritesContext'
 import SensorService from '@/Services/Sensor'
+import { apStyleTitleCase } from 'ap-style-title-case'
+import { DateTime } from 'luxon'
 
 export interface ISensorProps extends Omit<ISensor, 'type'> {}
 
 export default function Sensor (props: ISensorProps): ReactElement {
+  const [scrubPosition, setScrubPosition] = useState<IPlotScrubPosition | undefined>(undefined)
   const emptyPlot: IPlot = {
     id: `${props.datasetId}.${props.name}.line`,
     dimensions: {
@@ -23,11 +26,12 @@ export default function Sensor (props: ISensorProps): ReactElement {
       strokeColor: '#3b82f6ff',
       strokeWidth: 2
     },
+    scrubbing: true,
     type: EPlotTypes.line
   }
   const [plots, setPlots] = useState<IPlot[]>([])
 
-  function getData (): void {
+  const getData = (): void => {
     const newPlot = Object.assign({}, emptyPlot)
     newPlot.dataService = SensorService.getDataServiceForSensorChart(props)
     setPlots([newPlot])
@@ -35,22 +39,45 @@ export default function Sensor (props: ISensorProps): ReactElement {
 
   useEffect(getData, [])
 
+  const getPrettyYAxisName = () => {
+    return apStyleTitleCase(`${props.valueName} (${props.valueUnits})`.replace(/_/g, ' '))
+  }
+
+  const onScrub = (scrubPositions: IPlotScrubPositions) => {
+    setScrubPosition(Object.values(scrubPositions)[0])
+  }
+
+  const getScrubValueElement = (): ReactElement => {
+    if (scrubPosition === undefined) {
+      return <></>
+    }
+    const { xValue, yValue } = scrubPosition
+    const dateTime = DateTime.fromJSDate(new Date(xValue))
+    return (
+      <div className='text-blue-500 font-semibold flex gap-4 pl-4'>
+        <div className='w-[6rem]'>{dateTime.toFormat('hh:mm a')}</div>
+        <div>{Math.round((yValue + Number.EPSILON) * 100) / 100} {props.valueUnits}</div>
+      </div>
+    )
+  }
+
   if (plots.length > 0) {
     return (
       <div>
         <div className='flex justify-between pr-4 pt-4'>
-          <div className='text-md uppercase font-semibold text-slate-500 px-4 pt-4 break-words max-w-[calc(100vw-48px-12px)]'>{props.valueName} ({props.valueUnits})</div>
+          <div className='text-md uppercase font-semibold text-slate-500 px-4 pt-4 break-words max-w-[calc(100vw-48px-12px)]'>{getPrettyYAxisName()}</div>
           <FavoriteButton
             favorite={{
               ...props,
               type: 'sensor'
             }}
-          />
+            />
         </div>
+        {getScrubValueElement()}
         <Chart
-          // TODO: How to ensure that y axis labels are not cut off?
-          settings={{ width: 'auto', height: 300, margin: { left: 40, right: 40, bottom: 30, top: 10 } }}
+          settings={{ width: 'auto', height: 300, margin: { left: 50, right: 40, bottom: 30, top: 10 } }}
           plots={plots}
+          onScrub={onScrub}
         />
       </div>
     )
