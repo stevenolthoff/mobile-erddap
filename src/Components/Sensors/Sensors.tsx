856f3647e45c1ae941523ae61f3335f81fc1abf8
Steven Olthoff <steven@axiomdatascience.com>
2023-09-13 11:18:48 -0400

    break out functinon

--- src/Components/Sensors/Sensors.tsx
+++ src/Components/Sensors/Sensors.tsx
@@ -20,7 +20,7 @@ export default function Sensors (props: ISensorsProps) {
   const [emptySensors, setEmptySensors] = useState<string[]>([])
   const [nonEmptySensors, setNonEmptySensors] = useState<string[]>([])
 
-  useEffect(() => {
+  const loadData = () => {
     const sensorsWithUnits = Object.keys(sensors).filter(key => sensors[key].units)
     const dataFetchPromises = sensorsWithUnits.map(async key => {
       const valueName = sensors[key].units['Variable Name']
@@ -36,26 +36,27 @@ export default function Sensors (props: ISensorsProps) {
       const dataService = new DataService(dataServiceProps)
       return { id: key, result: await dataService.get() }
     })
-    Promise.all(dataFetchPromises).then((r) => {
-      console.log(r)
-      const empty: string[] = []
-      const nonEmpty: string[] = []
-      r.forEach(sensorResult => {
+    Promise.all(dataFetchPromises).then((sensors) => {
+      const idsOfEmptySensors: string[] = []
+      const idsOfNonEmptySensors: string[] = []
+      sensors.forEach(sensorResult => {
         const y = sensorResult.result.parsed?.accessors.y
         if (y !== undefined) {
           const data = sensorResult.result.parsed?.data
           const sensorIsEmpty = data?.filter(row => y(row) !== null).length === 0
           if (sensorIsEmpty) {
-            empty.push(sensorResult.id)
+            idsOfEmptySensors.push(sensorResult.id)
           } else {
-            nonEmpty.push(sensorResult.id)
+            idsOfNonEmptySensors.push(sensorResult.id)
           }
         }
       })
-      setEmptySensors(empty)
-      setNonEmptySensors(nonEmpty)
+      setEmptySensors(idsOfEmptySensors)
+      setNonEmptySensors(idsOfNonEmptySensors)
     }).catch(error => console.error(error))
-  }, [sensors, datasetId, station])
+  }
+
+  useEffect(loadData, [sensors, datasetId, station])
 
   const getSensorElements = (sensorIds: string[]): ReactElement[] => {
     return sensorIds.sort().map(id => {
