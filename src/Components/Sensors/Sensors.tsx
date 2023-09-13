d679a14fa64c898635ec7a9504680d1e7dc4f351
Steven Olthoff <steven@axiomdatascience.com>
2023-09-13 11:13:21 -0400

    move empty sensors to bottom

--- src/Components/Sensors/Sensors.tsx
+++ src/Components/Sensors/Sensors.tsx
@@ -1,9 +1,11 @@
 import Sensor from '@/Components/Sensor/Sensor'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import useMetadata from '@/Hooks/useMetadata'
 import TimeFrameService from '@/Services/TimeFrame'
 import { IStation } from '@/Contexts/FavoritesContext'
 import TimeFrame from '@/Components/TimeFrame/TimeFrame'
+import { DataService } from '@axdspub/axiom-ui-data-services'
+import SensorService from '@/Services/Sensor'
 
 interface ISensorsProps {
   station: IStation
@@ -15,29 +17,78 @@ export default function Sensors (props: ISensorsProps) {
   const [metadata] = useMetadata(datasetId)
   const { sensors } = metadata
   const timeFrame = TimeFrameService.getTimeFrame('past-week')
+  const [emptySensors, setEmptySensors] = useState<string[]>([])
+  const [nonEmptySensors, setNonEmptySensors] = useState<string[]>([])
 
-  const listItems = Object.keys(sensors).map(key => {
-    const sensor = sensors[key]
-    if (!sensor.units) {
-      return null
-    }
-    const valueName = sensors[key].units['Variable Name']
-    const valueUnits = sensors[key].units.Value
-    return <div key={key}>
-      <Sensor
-        name={key}
-        datasetId={datasetId}
-        valueName={valueName}
-        valueUnits={valueUnits}
-        timeFrame='past-week'
-        station={station}
-      />
-    </div>
-  }).filter(item => item !== null)
+  useEffect(() => {
+    const sensorsWithUnits = Object.keys(sensors).filter(key => sensors[key].units)
+    const dataFetchPromises = sensorsWithUnits.map(async key => {
+      const valueName = sensors[key].units['Variable Name']
+      const valueUnits = sensors[key].units.Value
+      const dataServiceProps = SensorService.getDataServiceForSensorChart({
+        name: key,
+        datasetId,
+        valueName,
+        valueUnits,
+        timeFrame: 'past-week',
+        station
+      })
+      const dataService = new DataService(dataServiceProps)
+      return { id: key, result: await dataService.get() }
+    })
+    Promise.all(dataFetchPromises).then((r) => {
+      console.log(r)
+      const empty: string[] = []
+      const nonEmpty: string[] = []
+      r.forEach(sensorResult => {
+        const y = sensorResult.result.parsed?.accessors.y
+        if (y !== undefined) {
+          const data = sensorResult.result.parsed?.data
+          const sensorIsEmpty = data?.filter(row => y(row) !== null).length === 0
+          if (sensorIsEmpty) {
+            empty.push(sensorResult.id)
+          } else {
+            nonEmpty.push(sensorResult.id)
+          }
+        }
+      })
+      setEmptySensors(empty)
+      setNonEmptySensors(nonEmpty)
+    }).catch(error => console.error(error))
+  }, [sensors, datasetId, station])
+
+  const getSensorElements = (sensorIds: string[]): ReactElement[] => {
+    return sensorIds.sort().map(id => {
+      const valueName = sensors[id].units['Variable Name']
+      const valueUnits = sensors[id].units.Value
+      return (
+        <div key={id}>
+          <Sensor
+            name={id}
+            datasetId={datasetId}
+            valueName={valueName}
+            valueUnits={valueUnits}
+            timeFrame='past-week'
+            station={station}
+          />
+        </div>
+      )
+    })
+  }
+
+  const getNonEmptySensors = (): ReactElement[] => {
+    return getSensorElements(nonEmptySensors)
+  }
+
+  const getEmptySensors = (): ReactElement[] => {
+    return getSensorElements(emptySensors)
+  }
+
   return <div className='overflow-hidden'>
     <TimeFrame timeFrame={timeFrame} />
     <div className='flex flex-col gap-4 divide-y pb-8'>
-      {listItems}
+      {getNonEmptySensors()}
+      {getEmptySensors()}
     </div>
   </div>
 }
